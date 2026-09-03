1ca4ef22c063e3c372d6856d50ddf383ec988276
ngotienloc <ngotienlocf@gmail.com>
2026-09-03 08:50:32 +0700

    feat: add protocol definitions and initial command IDs in protocol.h feat: add initial packet diagram for documentation

---
+++ bootloader/Core/Inc/protocol.h
@@ -0,0 +1,21 @@
+#ifndef __PROTOCOL_H
+#define __PROTOCOL_H
+
+#define PROTOCOL_HEADER_1 0xAA 
+#define PROTOCOL_HEADER_2 0x55
+#define PROTOCOL_TAIL     0x0D
+
+#define PROTOCOL_MAX_PAYLOAD_SIZE 260
+#define PROTOCOL_CHUNK_SIZE 256
+
+#define PROTOCOL_JUMP_KEY 0x55
+
+
+// Command IDs
+typedef enum {
+    CMD_START_UPDATE = 0x00, 
+    cmd
+}
+
+
+#endif /PROTOCOL_H */
